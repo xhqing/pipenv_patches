b857ee0b2aa4631df9990070af1d47d0d88e9602
xuhuaqing <huaqingxu89@gmail.com>
2022-08-15 17:34:19 +0800

    update

--- create_venv.py
+++ create_venv.py
@@ -17,4 +17,4 @@ if __name__ == "__main__":
     elif python_version == "python:3.9.12":
         os.system("pipenv --python /Users/mac/opt/anaconda3/envs/py39/bin/python3.9")
     else:
-        raise CommandError(f"""\033[01;31;01m `python create_venv.py python_version(python:3.9.12 or python:3.6.13)`\033[01;31;01m""")
+        raise CommandError(f"""\033[01;31;01m `python create_venv.py python_version("python:3.9.12" or "python:3.6.13")`\033[01;31;01m""")
