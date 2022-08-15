fd774842f38aa466dbdf25a2d0297d00a77c7495
xuhuaqing <huaqingxu89@gmail.com>
2022-08-15 17:40:00 +0800

    update

--- create_venv.py
+++ create_venv.py
@@ -4,17 +4,17 @@ from commonException import *
 if __name__ == "__main__":
     """
     args: 
-    python_version: "python:3.9.12", "python:3.6.13"
+    python_version: "py39", "py36"
     """
     try:
         python_version = sys.argv[1]
     except IndexError:
-        raise CommandError(f"""\033[01;31;01m `python create_venv.py python_version(python:3.9.12 or python:3.6.13)`\033[01;31;01m""")
+        raise CommandError(f"""\033[01;31;01m `python create_venv.py python_version("py39" or "py36")`\033[01;31;01m""")
 
     os.system("pipenv --rm")
-    if python_version == "python:3.6.13":
+    if python_version == "py36":
         os.system("pipenv --python /Users/mac/opt/anaconda3/envs/python36/bin/python3.6")
-    elif python_version == "python:3.9.12":
+    elif python_version == "py39":
         os.system("pipenv --python /Users/mac/opt/anaconda3/envs/py39/bin/python3.9")
     else:
-        raise CommandError(f"""\033[01;31;01m `python create_venv.py python_version("python:3.9.12" or "python:3.6.13")`\033[01;31;01m""")
+        raise CommandError(f"""\033[01;31;01m `python create_venv.py python_version("py39" or "py36")`\033[01;31;01m""")
